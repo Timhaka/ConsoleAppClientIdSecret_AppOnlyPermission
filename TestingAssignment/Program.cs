df1defa14f7c9fb6351c022c65b84286c35b0ee4
Timhaka <32096310+Timhaka@users.noreply.github.com>
2018-02-20 12:11:57 +0100

    Assignement testing

---
+++ TestingAssignment/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.SharePoint.Client;
+using OfficeDevPnP.Core;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://folkis2017.sharepoint.com/sites/Tim";
+            string clientId = ConfigurationManager.AppSettings["ClientId"];
+            string Secret = ConfigurationManager.AppSettings["ClientSecret"];
+
+            
+
+            AuthenticationManager authManager = new AuthenticationManager();
+            using (ClientContext ctx = authManager.GetAppOnlyAuthenticatedContext(url, clientId, Secret))
+            {
+                ctx.Load(ctx.Web);
+                ctx.ExecuteQuery();
+                Console.WriteLine("title before change " + ctx.Web.Title);
+
+
+                ctx.Web.Title = "App Changed The Title";
+                ctx.Web.Update();
+                ctx.ExecuteQuery();
+
+                Console.WriteLine("Title after change " + ctx.Web.Title);
+                Console.WriteLine(ctx.Web.Url);
+
+            }
+
+            Console.WriteLine("Press enter");
+            Console.ReadKey();
+
+
+        }
+    }
+}
